93fd2b16f64be354e1ac6062b26ee3f69f7e4d3b
Nikhil Bajpai <2022wb86522@wilp.bits-pilani.ac.in>
2026-09-21 21:50:19 +0530

    Initial Maven Project

---
+++ 2022WB86522/src/main/java/com/bits/App.java
@@ -0,0 +1,13 @@
+package com.bits;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
